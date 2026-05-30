a25133e032fb1c4731ed3fc4a47af7cdc8349c65
Bharat Sharma <bharatsharma@Bharats-MacBook-Air.local>
2026-05-31 02:18:44 +0530

    Resolve merge conflict

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from sklearn.tree import DecisionTreeRegressor
 
 from misc import (
@@ -32,6 +33,38 @@ def main():
 
     print(f"Decision Tree MSE: {mse}")
 
+from sklearn.tree import DecisionTreeRegressor
+
+from misc import (
+    load_data,
+    split_data,
+    train_model,
+    evaluate_model
+)
+
+def main():
+
+    df = load_data()
+
+    X_train, X_test, y_train, y_test = split_data(df)
+
+    model = DecisionTreeRegressor(
+        random_state=42
+    )
+
+    model = train_model(
+        model,
+        X_train,
+        y_train
+    )
+
+    mse = evaluate_model(
+        model,
+        X_test,
+        y_test
+    )
+
+    print(f"Decision Tree MSE: {mse}")
 
 if __name__ == "__main__":
     main()
