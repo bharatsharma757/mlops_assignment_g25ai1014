06060614315f9bcbf64d52fbbcbcebeebc80276c
Bharat Sharma <bharatsharma@Bharats-MacBook-Air.local>
2026-05-31 02:23:11 +0530

    Fix train.py

--- train.py
+++ train.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 from sklearn.tree import DecisionTreeRegressor
 
 from misc import (
@@ -25,46 +24,15 @@ def main():
         y_train
     )
 
-    mse = evaluate_model(
+    mse, r2 = evaluate_model(
         model,
         X_test,
         y_test
     )
 
     print(f"Decision Tree MSE: {mse}")
+    print(f"R2 Score: {r2}")
 
-from sklearn.tree import DecisionTreeRegressor
-
-from misc import (
-    load_data,
-    split_data,
-    train_model,
-    evaluate_model
-)
-
-def main():
-
-    df = load_data()
-
-    X_train, X_test, y_train, y_test = split_data(df)
-
-    model = DecisionTreeRegressor(
-        random_state=42
-    )
-
-    model = train_model(
-        model,
-        X_train,
-        y_train
-    )
-
-    mse = evaluate_model(
-        model,
-        X_test,
-        y_test
-    )
-
-    print(f"Decision Tree MSE: {mse}")
 
 if __name__ == "__main__":
     main()
